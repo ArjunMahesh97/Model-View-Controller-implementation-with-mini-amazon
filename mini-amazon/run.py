43772660fb79c02c9eacf84d19383adb45b0d6fa
ArjunMahesh97 <arjunmahesh97@gmail.com>
2018-04-08 14:28:34 +0530

    Add files via upload

---
+++ mini-amazon/run.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, Response
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.mini_amazon
+
+app = Flask('mini-amazon')
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    return 'healthy\n'
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return "<h1>Welcome to Mini Amazon</h1>"
+
+
+@app.route('/say-hello/<name>')
+def say_hello(name):
+    return '<h1>Hello, {0}!</h1>'.format(name)
+
+
+@app.route('/products', methods=['POST', 'GET'])
+def products():
+    if request.method == 'POST':
+        product = dict()
+        product['title'] = request.form['title']
+        product['description'] = request.form['description']
+        product['price'] = request.form['price']
+        print(product)
+        db.products_collection.insert_one(product)
+        return Response('OK\n', 200)
+    elif request.method == 'GET':
+        print(request.args['title'])
+        query = {
+            'title': request.args['title']
+        }
+        prods = db.products_collection.find(query)
+        matches = []
+        for prod in prods:
+            matches.append(prod)
+            print(prod)
+        return Response(str(matches), 200)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
